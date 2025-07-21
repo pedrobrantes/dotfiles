738b0cd5a0336468dbdb1750ddc1246fae6065ec
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-07-21 15:10:10 -0300

    refactor: modularize git and python configurations

--- home.nix
+++ home.nix
@@ -1,47 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  # Define a versão da configuração do home-manager.
-  # Isso ajuda a garantir a compatibilidade futura.
-  home.stateVersion = "25.05"; # Versão estável mais recente em 2025
+  home.stateVersion = "25.05";
 
   home.username = builtins.getEnv "USER";
 
   home.homeDirectory = builtins.getEnv "HOME";
 
-  
+  # Env
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
 
-  # Lista de pacotes que você quer instalar no seu perfil de usuário.
-  home.packages = with pkgs; [
-    # Exemplo: Adicione alguns utilitários básicos
-    # ripgrep
-    # fzf
-    # eza
+  # Programs modules
+  imports = [
+    ./programs/python.nix
+    ./programs/git.nix
   ];
-
-  # Configurações de programas (dotfiles)
-  # Exemplo: Configuração básica do Git
-  # programs.git = {
-  #   enable = true;
-  #   userName = "Seu Nome";
-  #   userEmail = "seu-email@exemplo.com";
-  #   extraConfig = {
-  #     init.defaultBranch = "main";
-  #   };
-  # };
-
-  # Exemplo: Configuração do Zsh
-  # programs.zsh = {
-  #   enable = true;
-  #   shellAliases = {
-  #     ll = "eza -l";
-  #     la = "eza -la";
-  #   };
-  #   initExtra = ''
-  #     # Comandos extras para o seu Zsh
-  #     export EDITOR=nvim
-  #   '';
-  # };
-
-  # Outras configurações...
+  
+  # Packages
+  home.packages = with pkgs; [
+  ];
 }

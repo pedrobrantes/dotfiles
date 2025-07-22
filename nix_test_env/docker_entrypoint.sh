6ace199d40fe8ec22e3a315889fa6baa475695da
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-07-22 02:16:22 -0300

    feat(ci): improved ci tests

--- nix_test_env/docker_entrypoint.sh
+++ nix_test_env/docker_entrypoint.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 export __HM_SESS_VARS_SOURCED="${__HM_SESS_VARS_SOURCED:-0}"
 
+ls -R /root/.nix-profile
+cat /root/.nix-channels 
+ls -R /root/.config
+
 source /root/.nix-profile/etc/profile.d/nix.sh
 
 if command -v home-manager &>/dev/null; then
@@ -16,5 +20,9 @@ export PATH="/root/.nix-profile/bin:$PATH"
 if [[ $# -eq 0 ]]; then
   set -- tail -f /dev/null
 fi
+
+ls -R /root/.nix-profile
+cat /root/.nix-channels 
+ls -R /root/.config
 
 exec "$@"

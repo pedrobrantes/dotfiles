8c2cc7e4cef923fa71c77b220af32b6e0be5ac16
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-07-22 02:08:34 -0300

    commit for ci build

--- nix_test_env/docker_entrypoint.sh
+++ nix_test_env/docker_entrypoint.sh
@@ -2,8 +2,16 @@
 set -euo pipefail
 
 export __HM_SESS_VARS_SOURCED="${__HM_SESS_VARS_SOURCED:-0}"
+
 source /root/.nix-profile/etc/profile.d/nix.sh
+
+if command -v home-manager &>/dev/null; then
+  home-manager switch || echo "home-manager switch fail"
+fi
+
 source /root/.nix-profile/etc/profile.d/hm-session-vars.sh
+
+export PATH="/root/.nix-profile/bin:$PATH"
 
 if [[ $# -eq 0 ]]; then
   set -- tail -f /dev/null

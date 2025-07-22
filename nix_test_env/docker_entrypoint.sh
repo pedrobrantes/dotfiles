5e2a0580e1ffbec662f1e8de54b358f0aa17af28
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-07-22 01:23:26 -0300

    feat(ci): add source nix env in entrypoint

--- nix_test_env/docker_entrypoint.sh
+++ nix_test_env/docker_entrypoint.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 export __HM_SESS_VARS_SOURCED="${__HM_SESS_VARS_SOURCED:-0}"
 
+source /root/.nix-profile/etc/profile.d/nix.sh
 source /root/.nix-profile/etc/profile.d/hm-session-vars.sh
 
 if [[ $# -eq 0 ]]; then

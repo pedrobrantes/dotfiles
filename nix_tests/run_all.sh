4801aeb20e6b4ee3b6081e3bdb51e29d200c6e9e
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-07-24 16:47:43 -0300

    commit of ci tests

--- nix_tests/run_all.sh
+++ nix_tests/run_all.sh
@@ -5,10 +5,12 @@ echo "--- Running All Container Tests ---"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+"$SCRIPT_DIR/load_nix_env.sh"
+
 find "$SCRIPT_DIR" -type f -name "*.sh" -not -name "$(basename "$0")" | while read -r test_script; do
   echo ""
   echo ">>> Executing test: ${test_script}"
-  /usr/local/bin/docker_entrypoint.sh "$test_script"
+  "$test_script"
 done
 
 echo ""

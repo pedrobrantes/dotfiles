a06de0d0fc5552c76c36c384f09d7a6fd3c4d369
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-17 02:10:28 -0300

    fix

--- programs/sops.nix
+++ programs/sops.nix
@@ -6,7 +6,7 @@
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
-    age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+    age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25520" ];
 
     # Every input is avaiable in 'config.sops.secrets.<nome>'.
     secrets = {

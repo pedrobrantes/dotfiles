3eb22f161369ac10fb8b504fbf1632c03099c3c4
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-17 19:30:55 -0300

    chore(bash): re-add nix.sh import

--- programs/bash.nix
+++ programs/bash.nix
@@ -1,68 +1,24 @@
-{ pkgs, config, ... }:
+{ pkgs, config, inputs, ... }:
 
 {
   programs.bash = {
     enable = true;
     enableCompletion = true;
 
-    bash-it = {
-      enable = true;
-      theme = "pure";
-    };
-
     initExtra = ''
-      # Evita que o script pare se não for interativo
-      case $- in
-        *i*) ;;
-          *) return;;
-      esac
-
-      # WSL Paths (do seu .bashrc)
-      export WIN_SYSTEM32="/mnt/c/windows/system32"
-      export WIN_USERPROFILE=$(wslpath "$(cmd.exe /c echo %USERPROFILE%)" | tr -d '\r')
-      export WIN_PROGRAM_FILES="/mnt/c/'program files'"
-      export PATH=$PATH:"$WIN_PROGRAM_FILES":"$WIN_SYSTEM32"
-
-      # Outras variáveis e configurações (do seu .bashrc e .profile)
-      unset MAILCHECK
-      export SCM_CHECK=true
-      export PIPX_USE_UV=1
-
-      # Sourcing de outros scripts e inicializações de ferramentas
-      [ -f ~/.bash_functions ] && . ~/.bash_functions
-      [ -f ~/.bash_aliases ] && . ~/.bash_aliases
-      [ -f ~/.fabric_configs ] && . ~/.fabric_configs
-      [ -f ~/.bash_api ] && . ~/.bash_api
-
-      # Fnm
-      FNM_PATH="${config.home.homeDirectory}/.local/share/fnm"
-      if [ -d "$FNM_PATH" ]; then
-        export PATH="$FNM_PATH:$PATH"
-        eval "$(fnm env)"
+      export BASH_IT="${inputs.bash-it}"
+      if [ -d "${config.home.homeDirectory}/.bash_it" ]; then
+        export BASH_IT_THEME='pure'
+        source "$BASH_IT/bash_it.sh"
       fi
-      
-      # Zoxide
-      eval "$(zoxide init bash)"
-
-      # Fzf - Carregando o script padrão
-      [ -f ~/.fzf.bash ] && source ~/.fzf.bash
-
-      # Brew
-      if [ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
-        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	
+      if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
       fi
-      
-      # Cargo
-      . "${config.home.homeDirectory}/.cargo/env"
-
-      # Ghcup
-      [ -f "${config.home.homeDirectory}/.ghcup/env" ] && . "${config.home.homeDirectory}/.ghcup/env"
-
-      # Juliaup
-      case ":$PATH:" in
-        *:${config.home.homeDirectory}/.juliaup/bin:*) ;;
-        *) export PATH=${config.home.homeDirectory}/.juliaup/bin''${PATH:+:${PATH}} ;;
-      esac
+	
+      if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+	. "$HOME/.nix-profile/etc/profile.d/nix.sh"
+      fi
     '';
   };
 

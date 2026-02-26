158f88ad3a16f369712627f071cbb463e2c414ac
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-02-26 21:57:18 +0000

    feat(direnv): add devenv integration with stdlib

--- programs/direnv.nix
+++ programs/direnv.nix
@@ -6,5 +6,13 @@
     nix-direnv.enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+
+    stdlib = ''
+      use_devenv() {
+        watch_file devenv.nix
+        watch_file devenv.yaml
+        eval "$(devenv print-dev-env)"
+      }
+    '';
   };
 }

056534b9018db1fc381bd55811856bdf1cd43566
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-05-17 20:12:17 +0000

    feat(machines): add tablet to aarch64/android

---
+++ machines/aarch64/android/tablet/default.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }:
+
+let
+  hostName = "aarch64.android.tablet";
+in
+{
+  imports = [ ../../../../home.nix ];
+
+  home.username = "brantes";
+  home.homeDirectory = "/home/brantes";
+
+  sops.defaultSopsFile = ../../../../secrets/secrets.yaml;
+  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+
+  sops.secrets."ssh_keys/tablet" = {
+    path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+    mode = "0600";
+  };
+
+  home.sessionVariables = {
+    HOSTNAME = hostName;
+  };
+
+  programs.zsh.initExtra = ''
+    export PROMPT="%F{yellow}%n%f@%F{green}${hostName}%f:%F{blue}%~%f%# "
+  '';
+
+  programs.bash.initExtra = ''
+    export PS1="\[\033[01;33m\]\u\[\033[00m\]@\[\033[01;32m\]${hostName}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+  '';
+
+  xdg.configFile."nix/nix.conf".text = ''
+    sandbox = false
+    connect-timeout = 20
+    max-jobs = 1
+  '';
+
+  programs.udocker.enable = true;
+}

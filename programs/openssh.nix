b3024bc85a30977c8c3648a4e5b9a46872a83853
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-07-22 19:13:26 -0300

    feat(ssh): add declarative device registry with tailscale nc proxyCommand

--- programs/openssh.nix
+++ programs/openssh.nix
@@ -1,21 +1,44 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  devices = {
+    smartphone = {
+      magicDns = "galaxy-s10";
+      sshPort = 8022;
+    };
+    tablet = {
+      magicDns = "pedros-tab-s6-lite";
+      sshPort = 8022;
+    };
+  };
+
+  tailscaleSocket = "${config.home.homeDirectory}/.tailscaled.sock";
+
+  mkSshBlock = alias: dev: {
+    hostname = dev.magicDns;
+    port = dev.sshPort;
+    proxyCommand = "${pkgs.tailscale}/bin/tailscale --socket=${tailscaleSocket} nc %h %p";
+  };
+
+  deviceBlocks = lib.mapAttrs mkSshBlock devices;
+
+  magicDnsNames = lib.concatStringsSep " "
+    (lib.mapAttrsToList (_: dev: dev.magicDns) devices);
+in
 {
-  home.packages = [
-    pkgs.openssh
-  ];
+  home.packages = [ pkgs.openssh ];
 
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks = {
+    matchBlocks = deviceBlocks // {
       "*.ts.net 100.*" = {
         user = "brantes";
         identityFile = "~/.ssh/id_ed25519";
       };
 
-      "galaxy-s10 pedros-tab-s6-lite smartphone tablet" = {
+      "${magicDnsNames}" = {
         port = 8022;
       };
 

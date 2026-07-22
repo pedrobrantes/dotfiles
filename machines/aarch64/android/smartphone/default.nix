4228a78ed8dc427f2c4e957178d2a7ba3cdacc01
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-07-22 14:54:57 -0300

    feat(ssh): add termux-boot and shell sshd auto-start for android devices

--- machines/aarch64/android/smartphone/default.nix
+++ machines/aarch64/android/smartphone/default.nix
@@ -23,11 +23,27 @@ in
 
   programs.zsh.initExtra = ''
     export PROMPT="%F{yellow}%n%f@%F{green}${hostName}%f:%F{blue}%~%f%# "
+    if ! pgrep -x "sshd" > /dev/null; then
+      ${pkgs.openssh}/bin/sshd -p 8022
+    fi
   '';
 
   programs.bash.initExtra = ''
     export PS1="\[\033[01;33m\]\u\[\033[00m\]@\[\033[01;32m\]${hostName}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+    if ! pgrep -x "sshd" > /dev/null; then
+      ${pkgs.openssh}/bin/sshd -p 8022
+    fi
   '';
+
+  home.file.".termux/boot/start-sshd.sh" = {
+    text = ''
+      #!/bin/sh
+      if ! pgrep -x "sshd" > /dev/null; then
+        ${pkgs.openssh}/bin/sshd -p 8022
+      fi
+    '';
+    executable = true;
+  };
 
   xdg.configFile."nix/nix.conf".text = ''
     sandbox = false

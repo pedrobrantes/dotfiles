dc7cfc3c418e1c59da298a5dfe6c7be32284ba62
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-07-22 16:23:56 -0300

    fix(ssh): set enableDefaultConfig to false to eliminate deprecation warning

--- programs/openssh.nix
+++ programs/openssh.nix
@@ -7,6 +7,7 @@
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     matchBlocks = {
       "*.ts.net 100.*" = {
@@ -16,6 +17,13 @@
 
       "galaxy-s10 pedros-tab-s6-lite smartphone tablet" = {
         port = 8022;
+      };
+
+      "*" = {
+        forwardAgent = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        compression = false;
       };
     };
   };

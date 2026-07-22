9e76857e37f6a0a2ddada0a4a169ac5a23463a6e
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-07-22 14:54:53 -0300

    feat(ssh): add global matchBlocks for tailscale nodes and port 8022

--- programs/openssh.nix
+++ programs/openssh.nix
@@ -1,7 +1,22 @@
-{ pkgs,  ... }:
+{ pkgs, ... }:
 
 {
   home.packages = [
     pkgs.openssh
   ];
+
+  programs.ssh = {
+    enable = true;
+
+    matchBlocks = {
+      "*.ts.net 100.*" = {
+        user = "brantes";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+
+      "galaxy-s10 pedros-tab-s6-lite smartphone tablet" = {
+        port = 8022;
+      };
+    };
+  };
 }
